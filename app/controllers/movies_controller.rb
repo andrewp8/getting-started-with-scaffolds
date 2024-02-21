3a644f840c3ab23af15e6c2169b25ee2920cd68f
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-02-21 18:11:42 +0000

    finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,13 @@
 class MoviesController < ApplicationController
+  def new
+    render template: "movies/new"
+  end
+
+  def edit
+    @the_movie = Movie.where(id: params.fetch(:id))[0]
+    render template: "movies/edit"
+  end
+
   def index
     matching_movies = Movie.all
 
@@ -18,32 +27,34 @@ class MoviesController < ApplicationController
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released", false)
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
+    if @the_movie.valid?
+      @the_movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      render template: "movies/with_errors"
     end
   end
 
   def update
     the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).first
+    @the_movie = Movie.where({ :id => the_id }).first
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released", false)
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+    if @the_movie.valid?
+      #debugger
+      @the_movie.save
+      redirect_to("/movies/#{@the_movie.id}", { :notice => "Movie updated successfully."} )
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => the_movie.errors.full_messages.to_sentence })
+      # redirect_to("/movies/#{the_movie.id/edit}", { :alert => "Movie not updated."} )
+      render "movies/edit"
     end
   end
 
